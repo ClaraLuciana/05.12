93581a24a440c61f1945879860f3ff05e8847980
Luciana Clara Silva <114404204+ClaraLuciana@users.noreply.github.com>
2024-12-05 15:16:33 -0300

    05.12

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ repetidas vezes.
 const carros = ["Gol","Fusca","Brasília","Del Rey","Chevette"];
 var tamanho = carros.length;
 
-for(let i=0; i<tamanho; i++){
-    document.getElementById('teste').innerHTML += carros[i] + " - ";
+for(let i = 0; i < tamanho; i++){
+    document.getElementById("teste").innerHTML +=carros[i] + " - ";
 }
